8dcd6c7bc6ccc8fa1f95867de4b52b18bdcf8a93
satishjpr <sathishhn2005@outlook.com>
2021-12-28 00:43:25 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InternalHealthCare.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Policy()
+        {
+            ViewBag.Message = "Your Policy page.";
+
+            return View();
+        }
+        public ActionResult Claims()
+        {
+            ViewBag.Message = "Your Policy page.";
+
+            return View();
+        }
+        public ActionResult Payments()
+        {
+            ViewBag.Message = "Your Policy page.";
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InternalHealthCare.UI.MvcApplication" Language="C#" %>
