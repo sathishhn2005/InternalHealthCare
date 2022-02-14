58c070407a6bc89ba7b1e404cbdf29af9b3b2b89
satishjpr <sathishhn2005@outlook.com>
2022-02-14 06:56:33 +0530

    GitRegentInternational

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace InternalHealthCare.UI.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page";
 
             return View();
         }
